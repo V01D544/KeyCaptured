03304eec58932b4a80c521ffc497864c564bd2b4
KUMAR SHUBHAM <93920333+V01D544@users.noreply.github.com>
2023-10-29 17:35:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 Program: KeyLogger (with Microphone, WebCamera, Screenshots, Audio Logging Feature)
-Author: Aman Kumar
-Date: 05/08/2022
+Author:  Kumar Shubham
+Date: 05/08/2023
 """
 
 # Libraries
